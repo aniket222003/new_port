fefe364154e3e0deaa9d3a63fa2e087e9b684f5e
Aniket <aniketsocial22@gmail.com>
2023-06-17 11:08:35 +0530

    changed gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*",
+      './pages/**/*.{html,js}',
+      './components/**/*.{html,js}',
+    
+  ],
+  theme: {
+    fontFamily:{
+      'roboto' : ['Roboto', 'sans-serif'],
+    },
+   
+  },
+  plugins: [
+    
+  ],
+}
+
